8b7cb90c70faea5a4cabf8f139a8f039de046fb7
NE420 <119597028+NE420@users.noreply.github.com>
2023-05-20 13:09:45 +0300

    Added GUI
    gui displays :-
    options for Difficulity and Algorithm .
    Start button .

--- AiProject/game.py
+++ AiProject/game.py
@@ -10,7 +10,7 @@ RED = 1
 BLUE = 2
 
 
-def main():
+def start(Method, Depth):
     board = Board()
     time.sleep(5)
     game_end = False
@@ -24,17 +24,44 @@ def main():
         print("--------------------")
         if game_end:
             break
-        # YOUR CODE GOES HERE
-        column,_ = board.AlphaBetaPruning(game_board,1,RED,-10000,10000)
-        #print(column)
-        # Insert here the action you want to perform based on the output of the algorithm
-        # You can use the following function to select a column
-
-        random_column = random.randint(0, 7)
+        if Method == min:
+            column, _ = board.AlphaBetaPruning(game_board, Depth, RED, -10000, 10000)
         board.select_column(column)
-
         time.sleep(2)
 
+def main():
+    import tkinter as tk
+
+    def option_selected():
+        selected_value = option_var.get()
+        selected_value2 = option_var2.get()
+
+        print("Selected Difficulity:", selected_value)
+        print("Selected Algorithm:", selected_value2)
+
+    root = tk.Tk()
+    root.geometry("400x200")
+    root.title("Connect 4")
+
+    root.configure(bg='firebrick')
+    option_var = tk.StringVar(root)
+    option_var.set("Difficulity")
+
+    option_menu1 = tk.OptionMenu(root, option_var, "Easy", "Medium", "Hard")
+    option_menu1.config(bg="grey")
+    option_menu1.pack()
+
+    option_var2 = tk.StringVar(root)
+    option_var2.set("Algorithm")
+
+    option_menu2 = tk.OptionMenu(root, option_var2, "MinMax", "Alpha Beta Pruning")
+    option_menu2.config(bg="grey")
+    option_menu2.pack()
+
+    button = tk.Button(root, text="Start", command=option_selected, bg="lime", fg="black")
+    button.pack()
+
+    root.mainloop()
 
 if __name__ == "__main__":
      main()
